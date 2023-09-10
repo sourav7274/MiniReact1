a345f507919298881a7a98e492ab67f59f676909
sourav7274 <55195415+sourav7274@users.noreply.github.com>
2023-09-10 21:15:22 +0530

    Add files via upload

---
+++ src/components/Tours.js
@@ -0,0 +1,21 @@
+import Card from "./Card";
+
+function Tours({tours, removeTour}){
+  return (
+    <div className="container">
+        <div>
+         <h2 className="title">Plan With Lob</h2>
+        </div>
+        <div className="cards">
+            {
+                tours.map( (tour) => {
+                    return <Card {...tour} removeTour={removeTour}></Card>
+                })
+            }
+        </div>
+    </div>
+  );
+
+}
+
+export default Tours;
